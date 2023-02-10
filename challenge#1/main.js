7e6d84e5a558422e51ed18e6108defbcd391e348
Thuc241203 <thucngo241203@gmail.com>
2023-02-10 20:59:05 +0700

    update challenge1

---
+++ challenge#1/main.js
@@ -0,0 +1,19 @@
+const log = console.log;
+
+const checkDogs = function(dogsJulia, dogsKate) {
+    const dogsJuliaCorrected = dogsJulia.slice();
+    dogsJuliaCorrected.splice(0, 1);
+    dogsJuliaCorrected.splice(-2);
+    const dogs = dogsJuliaCorrected.concat(dogsKate);
+    log(dogs);
+
+    dogs.forEach(function (dog, i){
+        if (dog >= 3){
+            log(`dog number ${i + 1} is an adult, and is ${dog} years old`);
+        } else {
+            log(`dog number ${i + 1} is still a puppy`);
+        }
+    });
+};
+
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
